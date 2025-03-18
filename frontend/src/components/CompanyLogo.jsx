93a52f7492c82cfed9c65068b7843440a2852990
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2025-03-18 09:54:14 +0530

    Added Logos

--- frontend/src/components/CompanyLogo.jsx
+++ frontend/src/components/CompanyLogo.jsx
@@ -1,55 +1,69 @@
 const CompanyLogo = () => {
-    const logos = [
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82d52566d454c99501c_Discord.svg", alt: "Discord", width: 130, height: 27 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82d52566d454c994fcd_NCR.svg", alt: "NCR", width: 98, height: 30 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82b52566d454c994910_mondaycom.svg", alt: "Monda.com", width: 134, height: 26 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82d52566d454c994fdb_Ted.svg", alt: "Ted", width: 56, height: 20 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82d52566d454c994fba_Dropbox.svg", alt: "Dropbox", width: 117, height: 24 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/6708139d2e30eb81beb54cc7_orangetheory-logo.svg", alt: "", width: 132, height: 32 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82d52566d454c994fb6_Greenhouse.svg", alt: "greenhouse", width: 112, height: 25 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82d52566d454c995018_Vice.svg", alt: "Vice", width: 90, height: 28 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82e52566d454c99502c_ideo-logo.svg", alt: "Ideo", width: 80, height: 22 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82d52566d454c994fb1_Philips.svg", alt: "Philips", width: 87, height: 16 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82d52566d454c994fd1_Mural.svg", alt: "Mural", width: 73, height: 22 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82e52566d454c995044_Name%3DABM%20Industries%2C%20Mode%3DLight.svg", alt: "ABM Industries", width: 65, height: 37 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82e52566d454c995028_TheNewYorkTimes.svg", alt: "The New York Times", width: 211, height: 28 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82c52566d454c994f5e_Upwork.svg", alt: "Upwork", width: 90, height: 28 },
-      { src: "https://cdn.prod.website-files.com/66e88746834b80507cdf7933/66e8a82e52566d454c995054_Decathlon.svg", alt: "decathlon logo", width: 160, height: 22 },
-    ];
-  
-    return (
-      <div className="relative bg-gradient-to-r from-red-900 via-gray-900 to-blue-700 overflow-hidden md:py-8 py-8">
+  const logos = [
+    { src: "https://res.cloudinary.com/dovdejenw/image/upload/v1742268610/Image%20Files/Marquee/tajpgulnccaywqi57mct.png", alt: "Discord", width: 600, height: 120, },
+    { src: "https://res.cloudinary.com/dovdejenw/image/upload/v1742268631/Image%20Files/Marquee/jllsywngxyklkxyxnesw.png", alt: "NCR", width: 600, height: 120 },
+    { src: "https://res.cloudinary.com/dovdejenw/image/upload/v1742268667/Image%20Files/Marquee/rtqt0eazegblisvwdcqy.png", alt: "Monda.com", width: 600, height: 120 },
+    { src: "https://res.cloudinary.com/dovdejenw/image/upload/v1742268687/Image%20Files/Marquee/zfu2p9gdqrxteqelxkhy.png", alt: "Ted", width: 600, height: 120 },
+    { src: "https://res.cloudinary.com/dovdejenw/image/upload/v1742268706/Image%20Files/Marquee/smgruuhsqewckwcovebb.png", alt: "Dropbox", width: 600, height: 120 },
+    { src: "https://res.cloudinary.com/dovdejenw/image/upload/v1742268729/Image%20Files/Marquee/tpmxw56zpyxz3wgiy1d2.png", alt: "", width: 600, height: 120 },
+    { src: "https://res.cloudinary.com/dovdejenw/image/upload/v1742269284/Image%20Files/Marquee/t8cljyhmtsnkbn3kjmo3.png", alt: "G", width: 600, height: 120 },
+    { src: "https://res.cloudinary.com/dovdejenw/image/upload/v1742269342/Image%20Files/Marquee/kic1f4n17b27ydmpeaim.png", alt: "H", width: 600, height: 120 },
+    { src: "https://res.cloudinary.com/dovdejenw/image/upload/v1742269370/Image%20Files/Marquee/w2qa2osuhdvgcsmyl2v7.png", alt: "I", width: 600, height: 120 },
+    { src: "https://res.cloudinary.com/dovdejenw/image/upload/v1742269391/Image%20Files/Marquee/qvyrfrmtfpq8mtylyn1w.png", alt: "J", width: 600, height: 120 },
+  ];
 
-
-        {/* Marquee animation container */}
-        <div className="flex animate-marquee">
-          {/* Double the logos array for seamless looping */}
-          {[logos, logos].map((logosSet, setIndex) => (
-            <div
-              key={setIndex}
-              className="flex flex-shrink-0 min-w-full items-center justify-around px-4"
-            >
-              {logosSet.map((logo, index) => (
-                <img
-                  key={`${setIndex}-${index}`}
-                  src={logo.src}
-                  alt={logo.alt}
-                  width={logo.width}
-                  height={logo.height}
-                  className="mx-8  object-contain flex-shrink-0"
-                  loading="lazy"
-                />
-              ))}
-            </div>
-          ))}
-        </div>
-  
-        {/* Gradient overlay */}
-        <div className="absolute inset-y-0 left-0 w-[20%] bg-gradient-to-r from-black to-transparent"></div>
-        <div className="absolute inset-y-0 right-0 w-[20%] bg-gradient-to-l from-black to-transparent"></div>
-    
+  return (
+    <div className="relative bg-gradient-to-r from-red-900 via-gray-900 to-blue-700 overflow-hidden py-8">
+      {/* Marquee animation container */}
+      <div
+        style={{
+          display: 'flex',
+          animation: 'marquee 60s linear infinite',  // Slower speed for smoother scrolling
+          whiteSpace: 'nowrap',  // Prevent wrapping for smooth scrolling
+        }}
+      >
+        {/* Duplicate the logos array for seamless looping */}
+        {[...logos, ...logos].map((logo, index) => (
+          <div
+            key={index}
+            style={{
+              flexShrink: 0,
+              display: 'flex',
+              justifyContent: 'center',
+              paddingLeft: '16px',
+              paddingRight: '16px',
+            }}
+          >
+            <img
+              src={logo.src}
+              alt={logo.alt}
+              width={logo.width}
+              height={logo.height}
+              style={{
+                objectFit: 'fill',
+                flexShrink: 1,
+              }}
+              loading="lazy"
+            />
+          </div>
+        ))}
       </div>
-    );
-  };
-  
-  export default CompanyLogo;
+
+      {/* Gradient overlay */}
+      <div
+        className="absolute inset-y-0 left-0 w-[20%]"
+        style={{
+          background: 'linear-gradient(to right, black, transparent)',
+        }}
+      ></div>
+      <div
+        className="absolute inset-y-0 right-0 w-[20%]"
+        style={{
+          background: 'linear-gradient(to left, black, transparent)',
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default CompanyLogo;
